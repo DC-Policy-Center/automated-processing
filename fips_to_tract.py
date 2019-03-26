607fe3d04e59a24e601f9d163eccc439fe38d50a
Michael Watson <mwatson10@gmail.com>
2019-03-26 14:09:41 -0400

    leg track

--- fips_to_tract.py
+++ fips_to_tract.py
@@ -1,10 +1,21 @@
 import pandas as pd
+import os
 
 
 def get_tract_from_fips(FIPS):
-    tract = FIPS[5:10]
-    return(tract)
+    try:
+        FIPS_to_parse = str(FIPS)
+        #t='{:f}'.format(float(FIPS_to_parse))
 
+        ff = '{:f}'.format(FIPS)
+        FIPS = str(ff)
+        tract_to_fill = [item for item in FIPS]
+        tract_to_format = ''.join(tract_to_fill[5:11])
+        tract = '{0:.2f}'.format(int(tract_to_format)/100)
+        #tract = tract.zfill(6)
+    except:
+        tract = 'err-tract-to-fips'
+    return(tract)
 
 
 data_folder_write = os.path.join("C:\\", "Users","mwatson","Documents","SharePoint","The DC Policy Center - Documents","Data","Nets Data","NETS Subsets","tract")
@@ -12,17 +23,31 @@ data_folder_read = os.path.join("C:\\", "Users","mwatson","Documents","SharePoin
 
 
 
-file_to_open = os.path.join(data_folder_write, "NETS2015_general_with_FIPS.csv")
-file_to_write = os.path.join(data_folder_write, "NETS2015_general_with_tract.csv")
-
+file_to_open = os.path.join(data_folder_write, "NETS2015_general_with_FIPS_7.csv")
+file_to_write = os.path.join(data_folder_write, "NETS2015_general_with_tract_2.csv")
+print('loading')
 df = pd.read_csv(file_to_open,encoding="ISO-8859-1")
+print('loaded')
+#df['tract'] = ''
 
-df['tract'] = ''
 
-for i in range(len(df)):
-    FIPS = df['FIPS'][i]
+#current_location_in_full_df = 0
+
+#print('starting loop')
+
+'''
+while current_location_in_full_df <= len(df)-1:
+    if current_location_in_full_df % 50000 == 0:
+        print('Currently at index {} of {}'.format(current_location_in_full_df,len(df)))
+
+    FIPS = df['FIPS'][current_location_in_full_df]
     tract = get_tract_from_fips(FIPS)
-    df['tract'][i] = tract
-
+    df.ix[current_location_in_full_df,'tract'] = tract
+    current_location_in_full_df += 1
+'''
+print('mapping')
+df["tract"] = df["FIPS"].map(get_tract_from_fips)
+print('mapped, \nwritting')
 
 df.to_csv(file_to_write,encoding="ISO-8859-1")
+print('done')
--- nets_locations.py
+++ nets_locations.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 import grequests
-
+import requests
 
 
 def url_builder(lat,lon):
@@ -18,13 +18,22 @@ def get_batch_FIPS(ll_list):
     return(fips_list)
 
 
+
+def get_FIPS(lat,lon):
+    fips_list = []
+    url = "https://geo.fcc.gov/api/census/block/find?latitude={}&longitude=-{}&showall=false&format=json".format(str(lat),str(lon)) #NEGATIVE BUILT INTO URL BUILDER FOR LONGITUDE
+    resp = requests.get(url)
+    FIPS = resp.json()['Block']['FIPS']
+    return(FIPS)
+
+
 # File locations
 data_folder_write = os.path.join("C:\\", "Users","mwatson","Documents","SharePoint","The DC Policy Center - Documents","Data","Nets Data","NETS Subsets","tract")
 data_folder_read = os.path.join("C:\\", "Users","mwatson","Documents","SharePoint","The DC Policy Center - Documents","Data","Nets Data","NETS Subsets","tract")
 
-file_to_open = os.path.join(data_folder_read, "NETS2015_general_with_empty_FIPS.csv")
-file_to_write_partial = os.path.join(data_folder_write, "NETS2015_general_with_FIPS_partial.csv")
-file_to_write = os.path.join(data_folder_write, "NETS2015_general_with_FIPS_2.csv")
+file_to_open = os.path.join(data_folder_read, "NETS2015_general_with_FIPS_6.csv")
+file_to_write_partial = os.path.join(data_folder_write, "NETS2015_general_with_FIPS_partial_7.csv")
+file_to_write = os.path.join(data_folder_write, "NETS2015_general_with_FIPS_7.csv")
 
 # reading in dataframe from CSV
 print('opening')
@@ -32,15 +41,15 @@ df = pd.read_csv(file_to_open,encoding="ISO-8859-1")
 
 master_fips_list = []
 
-batch_len = 1000 # A batch refers to one set of URLs to be pulled at once
+batch_len = 1 # A batch refers to one set of URLs to be pulled at once
 
 
-current_location_in_full_df = 0
+current_location_in_full_df = 1900000
 
 print('starting loop')
 num_left = len(df)
 
-while current_location_in_full_df <= len(df):
+while current_location_in_full_df <= len(df)-1:
     fips_list = []
     if current_location_in_full_df % 50000 == 0:
         print('Currently at index {} of {}'.format(current_location_in_full_df,len(df)))
@@ -48,25 +57,34 @@ while current_location_in_full_df <= len(df):
     if num_left < batch_len:
         batch_len = num_left
 
+        '''
     for batch_index in range(batch_len):
         url_list = []
         lat = df['Latitude'][current_location_in_full_df + batch_index]
         lon = df['Longitude'][current_location_in_full_df + batch_index]
         url = url_builder(lat,lon)
         url_list.append(url)
+        '''
+    lat = df['Latitude'][current_location_in_full_df]
+    lon = df['Longitude'][current_location_in_full_df]
+    FIPS = get_FIPS(lat,lon)
+    master_fips_list.append(FIPS)
+    #df['FIPS'][current_location_in_full_df] = FIPS
+    df.ix[current_location_in_full_df,'FIPS'] = FIPS
+    #fips_list = get_batch_FIPS(url_list)
+    #for item in fips_list: master_fips_list.append(item)
 
-    fips_list = get_batch_FIPS(url_list)
-    for item in fips_list: master_fips_list.append(item)
-
-
+    '''
     ii = current_location_in_full_df
     for ii in range(len(master_fips_list)):
         df['FIPS'][ii] = master_fips_list[ii]
         ii += 1
-
+        '''
     #print('incrementing')
-    current_location_in_full_df += 1000
-    if current_location_in_full_df % 300000 == 0:
+
+    current_location_in_full_df += batch_len
+    if current_location_in_full_df % 100000 == 0:
+        print('Writing partial')
         df.to_csv(file_to_write_partial,encoding="ISO-8859-1")
 
     num_left += current_location_in_full_df
